07d2e26d016dee599c637bb80de3c72a1d290ed0
葛乐 <>
2022-03-23 21:48:41 +0800

    [fix]color

--- src/components/menu/edit/tagBox.vue
+++ src/components/menu/edit/tagBox.vue
@@ -51,6 +51,33 @@ export default {
   methods: {
     getResourceColor(resource) {
       if (this.minder && this.minder.getResourceColor) {
+        if (resource == "通过" || resource == "Pass" || resource == "通過"){
+          return "#3bbf287a"
+        }
+        if (resource == "不通过" || resource == "UnPass" || resource == "不通過"){
+          return "#ed070780"
+        }
+        if (resource == "备注" || resource == "Remark" || resource == "備註"){
+          return "#e6a23c75"
+        }
+        if (resource == "失败" || resource == "Failure" || resource == "失敗"){
+          return "#DC143C"
+        }
+        if (resource == "阻塞" || resource == "Blocking" || resource == "阻塞"){
+          return "#1aade18f"
+        }
+        if (resource == "跳过" || resource == "Skip" || resource == "跳過"){
+          return "#c67cd7ab"
+        }
+        if (resource == "用例" || resource == "Case" || resource == "用例"){
+          return "#FFD700"
+        }
+        if (resource == "前置条件" || resource == "Prerequisite" || resource == "前置條件"){
+          return "#9933FA"
+        }
+        if (resource == "标签" || resource == "Tag" || resource == "標簽"){
+          return "#B0E0E6"
+        }
         return this.minder.getResourceColor(resource).toHEX();
       }
     },
